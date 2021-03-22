359b3fd46f81308c3d8097fa94f06263e40d9bc3
Ignacio R. de Almeida <ignacio.rdea@gmail.com>
2021-03-22 05:12:12 +0000

    AVL Tree Implementation Finished

--- AVL.cpp
+++ AVL.cpp
@@ -8,7 +8,6 @@ bool AVL::recursiveAdd(Node*& node, int data) {
 		//If Tree is empty
 	if (node == NULL){
 		node = new Node(data);
-		cout << "Insert new node " << node->data << endl;
 		return true;
 	} 
 		//If data already exists
@@ -31,36 +30,88 @@ bool AVL::recursiveAdd(Node*& node, int data) {
 	}
 }
 
+void AVL::replaceParent(Node*& temp, Node*& root){
+	if(root->right != NULL)
+		replaceParent(temp, root->right);
+	else {
+		temp->data = root->data;
+		temp = root;
+		root = root->left;
+	}
+}
+
+bool AVL::remove(int data){
+	return recursiveRemove(root, data);
+}
+
+bool AVL::recursiveRemove(Node*& node, int data){
+	bool rval;
+		//Base Condition - If node is empty
+	if(node == NULL){
+		return false;
+	}
+	else{
+			//Going to the left
+		if(data < node->data){
+			rval = recursiveRemove(node->left, data);
+			if(rval)
+				rebalance(node);
+			node->height = calculateHeight(node);
+			return rval;
+		}
+			//Going to the right
+		else if (data > node->data){
+			rval = recursiveRemove(node->right, data);
+			if(rval)
+				rebalance(node);
+			node->height = calculateHeight(node);
+			return rval;
+		}
+			//Node to be deleted found
+		else{
+			Node* temp = node;
+
+				//If the node doesn't have a lesser child
+			if(node->left == NULL){
+				node = node->right;
+			}
+				//If the node doesn't have a higher child
+			else if(node->right == NULL){
+				node = node->left;
+			}
+				//If node has both children	
+			else {
+				replaceParent(temp, temp->left);
+				rebalance(node);
+			}
+			delete temp;
+			return true;
+		}
+	}	
+}
 
 void AVL::rebalance(Node*& node){
 	int treeBalance = getBalance(node);
-	cout << "treeBalance: " << treeBalance << " on node " << node->data << endl;
  		//LL or LR tree
 	if (treeBalance < -1){
-		//int localLeftBalance = getBalance(node->left);
 			//LL
 		if(getBalance(node->left) == -1 || getBalance(node->left) == 0){
-			// cout << "LL" << endl;
 			rotateRight(node);
 		}
 			//LR
 		else if(getBalance(node->left) == 1){
-			// cout << "LR" << endl;
 			rotateLeft(node->left);
 			rotateRight(node);
 		}
 	}
 		//RR or RL
 	else if(treeBalance > 1){
-		//int localRightBalance = getBalance(node->right);
 			//RR
 		if(getBalance(node->right) == 1 || getBalance(node->right) == 0){
-			// cout << "RR" << endl;
 			rotateLeft(node);
 		}
 			//RL
 		else if(getBalance(node->right) == - 1){
-			// cout << "RL" << endl;
 			rotateRight(node->right);
 			rotateLeft(node);
 		}
@@ -83,15 +134,7 @@ int AVL::getBalance(Node*& node){
 	else{
 		return (calculateHeight(node->right) - calculateHeight(node->left));
 	}
-
 }
-
-// int AVL::get_height(Node* node){
-// 	if (node == nullptr)
-// 		return(0);
-// 	else
-// 		return node->getHeight();
-// }
 
 int AVL::calculateHeight(Node* node){
 	if(node == NULL){
@@ -108,35 +151,26 @@ int AVL::calculateHeight(Node* node){
 }
 
 void AVL::rotateRight(Node*& node){
-	cout << "In ---> rotateRight for: " << node->data << endl;
 	Node* temp = node->left;
   node->left = temp->right;
   temp->right = node;
   node = temp;
 
 	node->height = calculateHeight(node);
-
 }
 
 void AVL::rotateLeft(Node*& node){
-	cout << "In rotateLeft<---- for: " << node-> data << endl;
 	Node* temp = node;
 	node = node->right;
 	temp->right = node->left;
 	node->left = temp;
 
 	node->height = calculateHeight(node);
-
-}
-
-int AVL::max(int x, int y){
-	return (x > y)? x : y;
 }
 
 void AVL::printTree(Node*& node){
 	if(node == NULL)
 		return;
-	cout << "Value: " << node->data << " - Height: " << node->getHeight() << endl;
 	printTree(node->left);
 	printTree(node->right);
 }
@@ -145,70 +179,4 @@ void AVL::clear(){
 		while(root != NULL){
 		remove(root->data);
 	}
-}
-
-bool AVL::remove(int data){
-	return recursiveRemove(root, data);
-}
-
-bool AVL::recursiveRemove(Node*& node, int data){
-	bool rval;
-		//1. Base Condition - If node is empty
-	if(node == NULL){
-		return false;
-	}
-	else{
-			//2. Going to the left
-		if(data < node->data){
-			rval = recursiveRemove(node->left, data);
-			if(rval)
-				rebalance(node);
-			node->height = calculateHeight(node);
-			return rval;
-		}
-			//3. Going to the right
-		else if (data > node->data){
-			rval = recursiveRemove(node->right, data);
-			if(rval)
-				rebalance(node);
-			node->height = calculateHeight(node);
-			return rval;
-		}
-			//4. Node to be deleted found
-		else{
-			cout << "deleting node " << node->data << endl;
-			Node* temp = node;
-
-				//If the node doesn't have a lesser child
-			if(node->left == NULL){
-				// cout << "If node doesn't have lesser child " << endl;
-				node = node->right;
-			}
-				//If the node doesn't have a higher child
-			else if(node->right == NULL){
-				// cout << "If node doesn't have a higher child "<< endl;
-				node = node->left;
-			}
-				//If node has both children	
-			else {
-				// cout << "if node has both children" << endl;
-				replaceParent(temp, temp->left);
-				rebalance(node);
-			}
-			delete temp;
-			return true;
-		}
-	}	
-}
-
-
-void AVL::replaceParent(Node*& temp, Node*& root){
-	if(root->right != NULL)
-		replaceParent(temp, root->right);
-	else {
-		temp->data = root->data;
-		temp = root;
-		root = root->left;
-	}
-	
 }
--- AVL.h
+++ AVL.h
@@ -9,37 +9,32 @@ protected:
 public:
 	Node *root, *left, *right;
 	
-	AVL() {root = NULL;}//
-	virtual ~AVL() {clear();}//
+	AVL() {root = NULL;}
+	virtual ~AVL() {clear();}
 
-	Node* getRootNode() const{ return root;}//
+	Node* getRootNode() const{ return root;}
 
-	int getBalance(Node*& node);//
+	int getBalance(Node*& node);
 	
-	bool add(int data); //
+	bool add(int data);
 
-	bool recursiveAdd(Node*& node, int data);//
+	bool recursiveAdd(Node*& node, int data);
 
-	void replaceParent(Node*& temp, Node*& node);//
+	void replaceParent(Node*& temp, Node*& node);
 
-	bool remove(int data);//
+	bool remove(int data);
 
-	bool recursiveRemove(Node*& node, int data);//
+	bool recursiveRemove(Node*& node, int data);
 
-	void clear();//
+	void clear();
 
-	void rotateRight(Node*& node);//*Checked with example
+	void rotateRight(Node*& node);
 
-	void rotateLeft(Node*& node);//*Checked with example
+	void rotateLeft(Node*& node);
 
 	void rebalance(Node*& node);
 
-	void printTree(Node*& node);//
-
-	int max(int x, int y);//
+	void printTree(Node*& node);
 
 	int calculateHeight(Node* node);
-
-	// int get_height(Node* node);
-
 };
